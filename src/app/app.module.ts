4c4321e768bbbb813d23d4b45c14cae0670c2e99
HiltonRv <hrodriguezvila@gmail.com>
2019-11-04 20:56:03 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
+import { ListarComponent } from './components/listar/listar.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarComponent,
+    ListarComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
